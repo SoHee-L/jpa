80af2ac2d3d8e22db2af787dfd9dd42fe8dc88d1
SoHee-L <lsohee12@gmail.com>
2023-09-01 18:28:28 +0900

    [제목] 자바 ORM 표준 JPA 프로그래밍 - 기본편 / JPA 시작하기 / 수강 완료

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,26 @@
+package hellojpa;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity /*이걸 꼭 넣어야지 jpa가 인식하고 내가 관리해야되는 애라는것을 안다.*/
+public class Member {
+    @Id /*JPA에 PK가 뭔지 알려줘야되기 때문에 이것도 필수로 작성*/
+    private Long id;
+    private String name;
+    //Getter, Setter …
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+} /*자바class에 작성해서 관리할 수 있다.*/
